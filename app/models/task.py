4fe118055d1668f254f24636f22a754afd8cddc0
sashavershkova <a.m.vershkova@gmail.com>
2025-05-01 18:11:45 -0700

    add task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+
