b3c7ea216c01fcd49e4c15df8f501499c3e2251d
sashavershkova <a.m.vershkova@gmail.com>
2025-05-01 19:27:44 -0700

    add create_task, get_all_tasks

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app.routes.routes_helper_utilities import create_model_inst_from_dict_with_response
+from app.models.task import Task
+
+bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+# CREATE ONE TASK
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+    return create_model_inst_from_dict_with_response(Task, request_body)
+
+# READ ALL TASKS
+@bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(id)
+    tasks = db.session.scalars(query)
+    
+    response = []
+    for task in tasks:
+        response.append(task.to_dict())
+    
+    return response
+
+
+# READ ONE TASK BY ID
+
+# UPDATE ONE TASK
+
+# PATCH ONE TASK
+
+# DELETE ONE TASK
+
+# DELETE ALL TASKS
