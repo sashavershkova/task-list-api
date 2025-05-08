272f4f21827e283a10db3f41fffa1aa4b8527967
sashavershkova <a.m.vershkova@gmail.com>
2025-05-08 13:00:26 -0700

    add raletionship between task and goal

--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,21 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from datetime import datetime
+from typing import Optional
 from ..db import db
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+  from .goal import Goal
+
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped["Goal"] = relationship(back_populates="tasks")
 
     def to_dict(self):
         return {
